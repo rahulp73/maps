de5d9d98f35f69436affe06c89677699a4d6b506
rahulp73 <rahulpothumudi6@gmail.com>
2025-03-03 17:52:04 +0530

    Added React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react'
 import './App.css'
 import { createTheme, ThemeProvider } from '@mui/material';
 import Map from './Components/Map';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   const theme = createTheme({
     typography: {
-     "fontFamily": `"Roboto", "Arial" ,"Helvetica", sans-serif`,
-     "fontSize": 13,
-     "fontWeightLight": 300,
-     "fontWeightRegular": 400,
-     "fontWeightMedium": 500
+      "fontFamily": `"Roboto", "Arial" ,"Helvetica", sans-serif`,
+      "fontSize": 13,
+      "fontWeightLight": 300,
+      "fontWeightRegular": 400,
+      "fontWeightMedium": 500
     }
- });
+  });
 
   return <ThemeProvider theme={theme}>
-    <Map />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Map />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   </ThemeProvider>
 }
 
